31f2d20dab480f9a8aea3573d1ef051f3c635837
maheye <u277721157@byui.edu>
2024-02-11 05:40:29 +0800

    installed swagger

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', require('./routes'));
 
@@ -16,7 +26,6 @@ mongodb.initDb((err) => {
         console.log(err);
     } else {
         app.listen(port, () => {
-            console.log(`Database is listening and node Running on port ${port}`);
-        });
+            console.log(`Database is listening and node Running on port ${port}`)});
     }
 });
